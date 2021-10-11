537ad4c12433855d1b3453f26d803e3c995c4922
José D'Abruzzo Pereira <josep@dei.uc.pt>
2021-10-11 09:53:15 +0100

    Obtain list of commits from the vulnerable functions

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from cfg_extraction import extract_cfg
 from cfg_parsing import map_cfg_per_function
 from cfg_persistence import save_cfg
 import os
+import pandas as pd
 from version_management import load_commit
 
 target_commit = "3b365793c19aff95d1cf9bbea19f138752264d12"
-commits = [target_commit]
 
 base_project_directory = "/home/zema-21/Projects/"
+commit_data_directory = "function-data"
+commit_data_mask = "{}-functions.csv"
 
 
-def extract_cfg_per_commit(repository_path):
+def obtain_commits(project):
+    filepath = os.path.join(commit_data_directory, commit_data_mask.format(project))
+    df = pd.read_csv(filepath)
+    commits = df["Vulnerable Commit Hash"].tolist()
+    commits = list(set(commits))
+    return commits
+
+
+def extract_cfg_per_commit(repository_path, commits):
     for commit in commits:
         load_commit(commit)
         cfg_directory = extract_cfg(repository_path, commit)
@@ -21,7 +31,8 @@ def extract_cfg_per_commit(repository_path):
 def main():
     project = "httpd"
     repository_path = os.path.join(base_project_directory, project)
-    extract_cfg_per_commit(repository_path)
+    commits = obtain_commits(project)
+    extract_cfg_per_commit(repository_path, commits)
 
 
 if __name__ == "__main__":
