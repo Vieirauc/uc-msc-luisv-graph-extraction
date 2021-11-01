0903eee653cc7fb8680539d116bad87c57359052
José D'Abruzzo Pereira <josep@dei.uc.pt>
2021-11-01 11:49:39 +0000

    Execute the CFG extraction only for the commits that have not been extracted

--- cfg_extraction.py
+++ cfg_extraction.py
@@ -15,18 +15,20 @@ def extract_cfg(base_output_directory, project, repository_path, commit, run_per
     # joern-parse [repository_path]
     # joern-export --repr cfg --out [output_directory]
 
-    # Command to parse the source code
-    for directory in obtain_directories(repository_path, run_per_directory):
-        parse_command = "joern-parse {}".format(directory)
-        print(subprocess.Popen(parse_command, shell=True, stdout=subprocess.PIPE).stdout.read())
-
     graph_type = "cfg"
     output_directory = os.path.join(base_output_directory, project,
                                     "output-{}-{}".format(graph_type, commit))
 
-    # Command to export the cfg
-    # TODO still not sure if this should be done per directory
-    parse_command = "joern-export --repr {} --out {}".format(graph_type, output_directory)
-    print(subprocess.Popen(parse_command, shell=True, stdout=subprocess.PIPE).stdout.read())
+    # If the directory exists, the parsing was already performed for that commit
+    if not os.path.exists(output_directory):
+        # Command to parse the source code
+        for directory in obtain_directories(repository_path, run_per_directory):
+            parse_command = "joern-parse {}".format(directory)
+            print(subprocess.Popen(parse_command, shell=True, stdout=subprocess.PIPE).stdout.read())
+
+        # Command to export the cfg
+        # TODO still not sure if this should be done per directory
+        parse_command = "joern-export --repr {} --out {}".format(graph_type, output_directory)
+        print(subprocess.Popen(parse_command, shell=True, stdout=subprocess.PIPE).stdout.read())
 
     return output_directory
