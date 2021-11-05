5d78055458d405d808d1cfa0f265f13cb2198d31
José D'Abruzzo Pereira <josep@dei.uc.pt>
2021-11-05 15:39:12 +0000

    Initial source code to create the adjacency matrix

--- save_cfg_database.py
+++ save_cfg_database.py
@@ -1,4 +1,5 @@
 from cfg_parsing import read_graph
+import networkx as nx
 import os
 import pandas as pd
 
@@ -9,17 +10,51 @@ projects = ["httpd", "glibc", "gecko-dev", "linux", "xen"]
 CFG_FILE = "CFG_filepath"
 
 
+def convert_graph_to_adjacency_matrix(cfg):
+    adjacency_matrix = nx.adjacency_matrix(cfg)
+
+    print(cfg)  # prints the dot file
+    print(cfg.get_name())
+
+    for node in cfg.get_nodes():
+        print(node)
+        print(node.get_label())
+        print(node.get_name())
+    for edge in cfg.get_edges():
+        print(edge)
+        print(edge.get_source())
+        print(edge.get_destination())
+
+    # all nodes should have the label "CFG_Node", but only the  entry node should have the label "Function"
+    # CREATE(e: CFG_Node:Function
+    # {name: '1000133', label: '(METHOD,nsIndexedToHTML::Create)'})
+
+    # additional properties that a function has
+    # {commit, filepath, function_name, vulnerable}
+
+    # Regular node
+    # CREATE (e:CFG_Node {name: '1000146', label:'(<operator>.new,new nsIndexedToHTML())'})
+
+    # Create relationship (the relationship type RELTYPE can be changed)
+    # MATCH
+    #   (a:CFG_Node),
+    #   (b:CFG_Node)
+    # WHERE a.name = '1000133' AND b.name = '1000146'
+    # CREATE (a)-[r:RELTYPE]->(b)
+    # RETURN type(r);
+
+
 def read_cfg_file(project):
     filepath = os.path.join(data_directory, file_cfg_data_mask.format(project))
     df = pd.read_csv(filepath)
 
     for index, row in df.iterrows():
         cfg_filepath = row[CFG_FILE]
-        print(cfg_filepath)
         graphs = read_graph(cfg_filepath)
 
         if graphs is not None:
-            print(graphs[0])
+            cfg = graphs[0]
+            convert_graph_to_adjacency_matrix()
 
 
 def main():
