2f30dc70d28fe59bcc52465c525e1654389d0cdd
José D'Abruzzo Pereira <josep@dei.uc.pt>
2021-11-10 10:35:31 +0000

    Initial version to apply the graph convolution layers

--- save_cfg_database.py
+++ save_cfg_database.py
@@ -1,5 +1,7 @@
 from cfg_parsing import read_graph
+from graphviz import Source
 import networkx as nx
+import numpy as np
 import os
 import pandas as pd
 
@@ -10,9 +12,33 @@ projects = ["httpd", "glibc", "gecko-dev", "linux", "xen"]
 CFG_FILE = "CFG_filepath"
 
 
-def convert_graph_to_adjacency_matrix(cfg):
-    adjacency_matrix = nx.adjacency_matrix(cfg)
+def convert_graph_to_adjacency_matrix(cfg_dot):
+    cfg = nx.nx_pydot.from_pydot(cfg_dot)
+    for node in cfg_dot.get_nodes():
+        print(node.get_name(), node.get_label())
+    #print(type(cfg))
 
+    print(cfg.nodes())
+    print(cfg.edges())
+
+    #adjacency_matrix = nx.adjacency_matrix(cfg)
+    #print(type(adjacency_matrix))
+    #print(adjacency_matrix)
+
+    #np_matrix = nx.to_numpy_matrix(cfg, nodelist=cfg.nodes())
+    #print(np_matrix)
+    #print(cfg.nodes())
+
+    different_order_nodes = list(cfg.nodes())
+    head = different_order_nodes.pop(-2)
+    different_order_nodes.insert(0, head)
+    print(different_order_nodes)
+    np_matrix = nx.to_numpy_matrix(cfg, nodelist=different_order_nodes)
+    print(np_matrix)
+    return np_matrix
+
+
+def analyze_dot_cfg(cfg):
     print(cfg)  # prints the dot file
     print(cfg.get_name())
 
@@ -54,10 +80,83 @@ def read_cfg_file(project):
 
         if graphs is not None:
             cfg = graphs[0]
-            convert_graph_to_adjacency_matrix()
+            analyze_dot_cfg(cfg)
+
+
+def obtain_identity(A):
+    return np.identity(A.shape[0])
+
+
+def obtain_diagonal_matrix(A_tilde):
+    D = np.zeros(A_tilde.shape)
+    for i in range(D.shape[0]):
+        D[i][i] = np.sum(A_tilde[i, :])
+    return D
+
+
+def obtain_attribute_matrix(A):
+    X = np.zeros((A.shape[0], 2))
+    for i in range(X.shape[0]):
+        X[i][0] = np.sum(A[i, :])  # outdegree
+        X[i][1] = np.sum(A[:, i])  # indegree
+        #X[i][i] = np.sum(A[i, :])
+    return X
+
+
+def new_main():
+    cfg_filepath = os.path.join("cfg-data", "6-cfg.dot")
+    graphs = read_graph(cfg_filepath)
+
+    if graphs is not None:
+        cfg_dot = graphs[0]
+        print(type(cfg_dot))
+        #s = Source(cfg_dot, filename="test.gv", format="pdf")
+        #s.view()
+        A = convert_graph_to_adjacency_matrix(cfg_dot)
+        X = obtain_attribute_matrix(A)
+        calculate_graph_convolution_layer(A, X)
+
+
+def calculate_graph_convolution_layer(A, X, t=1):
+    I = obtain_identity(A)
+    A_tilde = A + I
+    print(A_tilde)
+    D_tilde = obtain_diagonal_matrix(A_tilde)
+    print(D_tilde)
+    W1 = np.matrix([[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]])
+    W2 = np.matrix([[0.0, 1.0, -2.0, 2.0], [1.0, 1.0, 7.0, -2.0], [1.0, 0.0, -1.0, 4.0]])
+    W = [W1, W2]
+    print(W1)
+    print(W2)
+    print(X)
+    # Zt+1 = f(D_tilde ^-1 A_tilde Zt Wt)
+    # Z0 = X
+    D_inv = np.linalg.inv(D_tilde)
+    print("D_inv\n", D_inv)
+    D_A = np.dot(D_inv, A_tilde)
+    print("D_A\n", D_A)
+    D_A_X = np.dot(D_A, X)
+    print("D_A_X\n", D_A_X)
+    Ztp1 = np.dot(D_A_X, W[t-1])
+    Ztp1 = np.maximum(Ztp1, 0.0)
+    print("Ztp1\n", Ztp1, "\n", Ztp1.shape)
+    return Ztp1
 
 
 def main():
+    new_main()
+    A = np.matrix([[0.0, 1.0, 0.0, 1.0, 0.0],
+                   [0.0, 0.0, 1.0, 0.0, 0.0],
+                   [0.0, 0.0, 0.0, 1.0, 1.0],
+                   [0.0, 0.0, 0.0, 0.0, 0.0],
+                   [0.0, 0.0, 0.0, 0.0, 0.0]])
+    print(type(A))
+    X = np.matrix([[2.0, 3.0], [1.0, 5.0], [7.0, 4.0], [8.0, 9.0], [6.0, 1.0]])
+    Z1 = calculate_graph_convolution_layer(A, X)
+    Z2 = calculate_graph_convolution_layer(A, Z1, t=2)
+    Z1_2 = np.concatenate((Z1, Z2), axis=1)
+    print(Z1_2)
+    return
     for project in projects[2:3]:
         read_cfg_file(project)
 
