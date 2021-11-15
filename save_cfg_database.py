83628d4110fa1ebe00bc052fd84ffd437f8dd169
José D'Abruzzo Pereira <josep@dei.uc.pt>
2021-11-15 11:27:18 +0000

    Refactoring for the feature extraction of the CFG

--- save_cfg_database.py
+++ save_cfg_database.py
@@ -106,7 +106,6 @@ def obtain_attribute_matrix(A):
     for i in range(X.shape[0]):
         X[i][0] = np.sum(A[i, :])  # outdegree
         X[i][1] = np.sum(A[:, i])  # indegree
-        #X[i][i] = np.sum(A[i, :])
     return X
 
 
@@ -132,41 +131,42 @@ def calculate_graph_convolution_layer(A, X, t=1):
     print("D_A\n", D_A)
     D_A_X = np.dot(D_A, X)
     print("D_A_X\n", D_A_X)
-    Ztp1 = np.dot(D_A_X, W[t-1])
+    Ztp = np.dot(D_A_X, W[t-1])
 
     # f: nonlinear activation function (RELU)
-    Ztp1 = np.maximum(Ztp1, 0.0)
-    print("Ztp1\n", Ztp1, "\n", Ztp1.shape)
+    Ztp = np.maximum(Ztp, 0.0)
+    print("Ztp{}\n".format(t), Ztp, "\n", Ztp.shape)
 
     # Ztp is the output activation matrix
-    return Ztp1
+    return Ztp
 
 
-def new_main():
-    cfg_filepath = os.path.join("cfg-data", "6-cfg.dot")
+def extract_data_from_file(cfg_directory, cfg_filename):
+    graph, X, Z1_t = None, None, None
+
+    cfg_filepath = os.path.join(cfg_directory, cfg_filename)
     graphs = read_graph(cfg_filepath)
 
     if graphs is not None:
         cfg_dot = graphs[0]
         print(type(cfg_dot))
-        #s = Source(cfg_dot, filename="test.gv", format="pdf")
-        #s.view()
+        # s = Source(cfg_dot, filename="test.gv", format="pdf")
+        # s.view()
 
         # TODO what if we do a topological sorting?
         A = convert_graph_to_adjacency_matrix(cfg_dot)
         X = obtain_attribute_matrix(A)
-        calculate_graph_convolution_layer(A, X)
+
+        Z1_t = obtain_graph_convolution_layers(A, X)
+        print("Z1:t of graph from file\n", Z1_t)
 
         cfg_nx = nx.nx_pydot.from_pydot(cfg_dot)
         graph = dgl.convert.from_networkx(cfg_nx)
-        print(type(graph))
-        print(graph)
-        sortpool = SortPooling(k=2)
+    return graph, X, Z1_t
 
 
-def obtain_graph_convolution_layers(A, X):
+def obtain_graph_convolution_layers(A, X, max_t=2):
     Z = []
-    max_t = 2
     Zi_minus1 = X
     for i in range(0, max_t):
         Zi = calculate_graph_convolution_layer(A, Zi_minus1, t=i + 1)
@@ -179,14 +179,12 @@ def obtain_graph_convolution_layers(A, X):
     return Z1_t
 
 
-def main():
-    new_main()
+def obtain_sample_data():
     A = np.matrix([[0.0, 1.0, 0.0, 1.0, 0.0],
                    [0.0, 0.0, 1.0, 0.0, 0.0],
                    [0.0, 0.0, 0.0, 1.0, 1.0],
                    [0.0, 0.0, 0.0, 0.0, 0.0],
                    [0.0, 0.0, 0.0, 0.0, 0.0]])
-    print(type(A))
     X = np.matrix([[2.0, 3.0], [1.0, 5.0], [7.0, 4.0], [8.0, 9.0], [6.0, 1.0]])
     Z1_t = obtain_graph_convolution_layers(A, X)
     print(Z1_t)
@@ -196,17 +194,37 @@ def main():
     dst_ids = th.tensor([i - 1 for i in [2, 4, 3, 4, 5]])
     g1 = dgl.graph((src_ids, dst_ids))
 
+    return g1, X, Z1_t
+
+
+def extract_sortpooling_layer(graph, Z1_t_th):
     # O k aqui é o quantidade de vértices que serão utilizados.
     # No artigo Zhang2018, o valor é definido como 60% dos grafos tendo mais de k vértices
     sortpool = SortPooling(k=3)
-    g1_feat = th.from_numpy(Z1_t)
-    feats = sortpool(g1, g1_feat)
-    print("AdaptiveMaxPool1D")
-    m = nn.AdaptiveMaxPool1d(3) # THIS ALMOST WORK, BUT IT HAS A DIFFERENT STRIDE IN THE PAPER!
-    output = m(g1_feat)
-    print("output (AdaptiveMaxPool1d)\n", output)
+    sortpool_feats = sortpool(graph, Z1_t_th)
+    print("sortpool_feats\n", sortpool_feats)
+    return sortpool_feats
 
-    print("feats (application of SortPooling)\n", feats)
+
+def extract_adaptive_max_pool(Z1_t_th):
+    m = nn.AdaptiveMaxPool1d(3)  # THIS ALMOST WORK, BUT IT HAS A DIFFERENT STRIDE IN THE PAPER!
+    amp = m(Z1_t_th)
+    print("amp (AdaptiveMaxPool1d)\n", amp)
+    return amp
+
+
+def main():
+    graph, X, Z1_t = extract_data_from_file("cfg-data", "6-cfg.dot")
+    Z1_t_th = th.from_numpy(Z1_t)
+    extract_sortpooling_layer(graph, Z1_t_th)
+    extract_adaptive_max_pool(Z1_t_th)
+
+
+    graph, X, Z1_t = obtain_sample_data()
+    Z1_t_th = th.from_numpy(Z1_t)
+    extract_sortpooling_layer(graph, Z1_t_th)
+    extract_adaptive_max_pool(Z1_t_th)
+
     return
     for project in projects[2:3]:
         read_cfg_file(project)
